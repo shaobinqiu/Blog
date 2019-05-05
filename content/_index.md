73d8fb4728e24ccdc3f9e499ab0d5eb703c9e4ab
shaobinqiu <18814122627@163.com>
2019-05-05 16:56:36 +0800

    rebuilding site Sun May  5 16:56:29 CST 2019

--- content/_index.md
+++
@@ -1,6 +0,0 @@
----
-title: "Dao"
-featured_image: '/images/gohugo-default-sample-hero-image.jpg'
-description: "春有百花， 秋有月； 夏有凉风， 冬有雪."
----
-一个人懒太久了，稍微努力一下就觉得自己在拼命...
---
+++ themes/even/src/js/main.js
@@ -0,0 +1,21 @@
+import {Even} from './even.js'
+
+import '../css/style.scss'
+
+$(document).ready(function () {
+  Even.backToTop();
+  Even.mobileNavbar();
+  Even.toc();
+  Even.fancybox();
+});
+
+Even.flowchart();
+Even.sequence();
+
+if (window.hljs) {
+  hljs.initHighlighting();
+  Even.highlight();
+} else {
+  Even.chroma();
+}
+
